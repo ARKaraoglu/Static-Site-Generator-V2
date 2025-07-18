f1f0dd2672fa876e5f403b349ebe1eb9c68d79d5
ARKaraoglu <ahmetrusenkara@gmail.com>
2025-07-17 20:31:16 -0400

    Added text_node_to_html_node function to textnode.py Added tests for the added function
    Changes to be committed:
    	modified:   src/test_textnode.py
    	modified:   src/textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from htmlnode import HTMLNode, LeafNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -46,6 +47,61 @@ class TestTextNode(unittest.TestCase):
     def test_repr5(self):
         node = TextNode("This is a TextNode", TextType.TEXT, "#")
         self.assertEqual(node.__repr__(), "TextNode(This is a TextNode, text, #)")
+
+    def test_textnode_to_html(self):
+        textnode = TextNode("This is a text", TextType.TEXT)
+        htmlnode = text_node_to_html_node(textnode)
+        
+        assert isinstance(htmlnode, LeafNode)
+
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode.tag, None)
+        self.assertEqual(htmlnode.value, "This is a text")
+
+    def test_textnode_to_html2(self):
+        textnode = TextNode("This is a bold", TextType.BOLD)
+        htmlnode = text_node_to_html_node(textnode) 
+        
+        assert isinstance(htmlnode, LeafNode)
+        
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode.to_html(), "<b>This is a bold</b>")
+
+    def test_textnode_to_html3(self):
+        textnode = TextNode("This is an italic", TextType.ITALIC)
+        htmlnode = text_node_to_html_node(textnode)
+        
+        assert isinstance(htmlnode, LeafNode)
+        
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode.to_html(), "<i>This is an italic</i>")
+
+    def test_textnode_to_html4(self):
+        textnode = TextNode("This is a code", TextType.CODE)
+        htmlnode = text_node_to_html_node(textnode)
+        
+        assert isinstance(htmlnode, LeafNode)
+        
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode.to_html(), "<code>This is a code</code>")
+
+    def test_textnode_to_html5(self):
+        textnode = TextNode("This is a link", TextType.LINK, "#")
+        htmlnode = text_node_to_html_node(textnode)
+        
+        assert isinstance(htmlnode, LeafNode)
+        
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode.to_html(), '<a href="#">This is a link</a>')
+
+    def test_textnode_to_html6(self):
+        textnode = TextNode("This is an image", TextType.IMAGE, "/")
+        htmlnode = text_node_to_html_node(textnode)
+        
+        assert isinstance(htmlnode, LeafNode)
+        
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode.to_html(), '<img src="/" alt="This is an image"></img>')
 
 
 
